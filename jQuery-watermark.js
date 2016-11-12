0a3d1a83158cdcbc12da576e14e8dbf6122a7ba7
Zerguine <zerguinegabriel@gmail.com>
2016-11-12 19:02:23 +0100

    Create jQuery-watermark.js

---
+++ jQuery-watermark.js
@@ -0,0 +1,17 @@
+
+
+    $('.watermark').watermark({
+        text: 'GOOGLE.COM',
+        textWidth: 100,
+        gravity: 'w',
+        opacity: 1,
+        margin: 12
+    });
+
+    $('.watermark2').watermark({
+        path: 'http://i.imgur.com/KTrvBm5.png',
+        margin: 0,
+        gravity: 'nw',
+        opacity: 0.5,
+        outputWidth: 400
+    });
